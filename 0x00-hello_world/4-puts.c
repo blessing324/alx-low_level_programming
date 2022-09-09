1fe0888c89120c163e50a7e67667f259e49fa60a
blessing324 <idowublessingj@gmail.com>
2022-09-08 21:46:46 -0700

    All the tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+/**
+ * main - print the phrase "Programmimg is *like building a multilingual puzzle"
+ *return - zero if edited propery, non-ze  *ro otherwise
+ */
+
+int main(void)
+{
+	puts("\"Programming is like buildi        ng a multilingual puzzle);
+	return (0);
+}
